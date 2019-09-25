29bd595d6b27f8f16b77f4a683b6852e4251175f
danilopez97 <estu97dani@gmail.com>
2019-09-25 01:34:54 -0600

    plantilla integrada

---
+++ resources/views/admin/parciales/menu.blade.php
@@ -0,0 +1,21 @@
+<ul class="sidebar-menu" data-widget="tree">
+        <li class="header">Navegacion</li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="#"><i class="fa fa-dashboard"></i> <span>Inicio</span></a></li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Presolicitud</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Ejecutores</a></li>
+            <li><a href="#">Cargo</a></li>
+            <li><a href="#">Tipo de Obra</a></li>
+            <li><a href="#">Solicitud</a></li>
+            <li><a href="#">Personas</a></li>
+            <li><a href="#">Estado licencia</a></li>
+            <li><a href="#">Estado factibilidad</a></li>
+          </ul>
+        </li>
+      </ul>
