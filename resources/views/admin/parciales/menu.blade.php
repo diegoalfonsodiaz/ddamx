eb7336f0315a960de9c01b0a9f74fe2038638eef
Diego <diegoalfonsodiaz@gmail.com>
2019-09-25 16:32:41 -0600

    Agregando Menu

--- resources/views/admin/parciales/menu.blade.php
+++ resources/views/admin/parciales/menu.blade.php
@@ -2,6 +2,7 @@
         <li class="header">Navegacion</li>
         <!-- Optionally, you can add icons to the links -->
         <li class="active"><a href="#"><i class="fa fa-dashboard"></i> <span>Inicio</span></a></li>
+
         <li class="treeview">
           <a href="#"><i class="fa fa-link"></i> <span>Presolicitud</span>
             <span class="pull-right-container">
@@ -18,4 +19,56 @@
             <li><a href="#">Estado factibilidad</a></li>
           </ul>
         </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Licencias</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Licencias</a></li>
+            <li><a href="#">Estado licencias</a></li>
+            <li><a href="#">Tipo de via </a></li>
+          </ul>
+        </li>
+
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Denuncias</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Denuncias </a></li>
+            <li><a href="#">Estado Denuncias</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Usuarios</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Usuarios </a></li>
+            <li><a href="#">Rol</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Bitacora</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Bitacora  </a></li>
+            <li><a href="#">Asunto de Bitacora</a></li>
+          </ul>
+        </li>
+
+        
       </ul>
