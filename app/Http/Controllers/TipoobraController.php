6792890c1fc1dbd03a1e343cc5c41884fd9a6634
Luis <angelcoyoy713@outlook.com>
2019-09-26 17:02:23 -0600

    Migracion

---
+++ app/Http/Controllers/TipoobraController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tipoobra;
+use Illuminate\Http\Request;
+
+class TipoobraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tipoobra  $tipoobra
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tipoobra $tipoobra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tipoobra  $tipoobra
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tipoobra $tipoobra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tipoobra  $tipoobra
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tipoobra $tipoobra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tipoobra  $tipoobra
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tipoobra $tipoobra)
+    {
+        //
+    }
+}
