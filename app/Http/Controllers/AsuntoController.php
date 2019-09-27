ab25687e0854e76d0f35ddaeeb4268dce54601ed
Diego <diegoalfonsodiaz@gmail.com>
2019-09-26 22:14:08 -0600

    creadoModelo

---
+++ app/Http/Controllers/AsuntoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Asunto;
+use Illuminate\Http\Request;
+
+class AsuntoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Asunto  $asunto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Asunto $asunto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Asunto  $asunto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Asunto $asunto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Asunto  $asunto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Asunto $asunto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Asunto  $asunto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Asunto $asunto)
+    {
+        //
+    }
+}
