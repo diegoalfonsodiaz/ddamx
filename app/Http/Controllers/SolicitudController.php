2cd7e89cd97f4ecca353162e2505cc0f9f761854
Diego <diegoalfonsodiaz@gmail.com>
2019-09-28 15:33:56 -0600

    modelocreado

---
+++ app/Http/Controllers/SolicitudController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Solicitud;
+use Illuminate\Http\Request;
+
+class SolicitudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Solicitud  $solicitud
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Solicitud $solicitud)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Solicitud  $solicitud
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Solicitud $solicitud)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Solicitud  $solicitud
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Solicitud $solicitud)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Solicitud  $solicitud
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Solicitud $solicitud)
+    {
+        //
+    }
+}
