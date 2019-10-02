29513841a91eef45560e75495c729c998af53cfa
Diego <diegoalfonsodiaz@gmail.com>
2019-10-01 21:27:32 -0600

    validacion

--- resources/views/cargoejecutor/create.blade.php
+++ resources/views/cargoejecutor/create.blade.php
@@ -1,10 +1,21 @@
 @extends('admin.principal')
 @section('contenido')
+
+<?
+@if($errors->any()) 
+    <div class="alert alert-danger"></div>
+    @foreach($errors->all() as $error)
+    <li>{{ $error }}</li>
+@endif
+>
+
 <div class="box box-primary">
             <div class="box-header with-border">
               <h3 class="box-title">Crear Cargo ejecutor</h3>
             <!-- /.box-header -->
             <!-- form start -->
+
+            
             <form action="{{ route('cargoejecutor.store') }}" method="POST">
     @csrf
   
