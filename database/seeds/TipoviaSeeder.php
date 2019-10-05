d1980747d527768e75ac9a234ef3b134bb96f552
andrecastle <racastillo705@gmail.com>
2019-10-04 21:58:44 -0600

    SeedTipoVia

---
+++ database/seeds/TipoviaSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TipoviaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tipovias')->insert([
+            'nombre' => 'Tierra',
+            'descripcion' => 'Autorizada',
+            'estado' => '1',
+        ]);
+
+        DB::table('tipovias')->insert([
+            'nombre' => 'Empedrado',
+            'descripcion' => 'Autorizada',
+            'estado' => '1',
+        ]);
+
+        DB::table('tipovias')->insert([
+            'nombre' => 'Adoquin',
+            'descripcion' => 'Autorizada',
+            'estado' => '1',
+        ]);
+
+        DB::table('tipovias')->insert([
+            'nombre' => 'Asfalto',
+            'descripcion' => 'Autorizada',
+            'estado' => '1',
+        ]);
+
+        DB::table('tipovias')->insert([
+            'nombre' => 'Pavimento',
+            'descripcion' => 'Autorizada',
+            'estado' => '1',
+        ]);
+    }
+}
