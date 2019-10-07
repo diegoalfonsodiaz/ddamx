9b1b3c9344b45211742fdadfbb6ae43a8405135c
Diego <diegoalfonsodiaz@gmail.com>
2019-10-06 18:51:15 -0600

    antesdemerge

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        
-        
+
         
         $this->call(EstadofactibilidadTableSeeder::class);
-=======
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(PersonasTableSeeder::class);
->>>>>>> 8e0f5eae335661c348957ea23e57b9e31b49b797
         $this->call(CargoejecutoresTableSeeder::class);
         $this->call(EjecutorSeeder::class);
         $this->call(AsuntoSeeder::class);
