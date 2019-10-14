d4e5233af0c3e957af68a13e49d149ae680b8b76
Diego <diegoalfonsodiaz@gmail.com>
2019-10-13 23:16:19 -0600

    antes del merge

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,9 +20,12 @@
                 @endif
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        correo: admin@gmail.com contraseña:administrador
+                            
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
+                           
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
