aa07754db8d355044187239c3d63f9d4e0e91e6c
andrecastle <racastillo705@gmail.com>
2019-10-27 18:47:20 -0600

    datos full

--- resources/views/estadolicencia/agregar.blade.php
+++ resources/views/estadolicencia/agregar.blade.php
@@ -34,7 +34,7 @@
                 </div>
               
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Descripcion</label>
+                  <label for="exampleInputEmail1">Descripción</label>
                   <input type="text" name="descripcion" class="form-control" placeholder="Ingresar Estado Licencia">
                 </div>
                 
