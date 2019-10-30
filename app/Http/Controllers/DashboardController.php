3753c02dcfd6f8aeb1038410dd96f917b733fd02
andrecastle <racastillo705@gmail.com>
2019-10-30 02:35:00 -0600

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,8 +21,14 @@ $fecha=date("Y-m-d",strtotime($fecha_actual."- 1 days"));
         //consulta personas activas
         $consultapersona= DB::table('personas')
         ->select('personas.id')
-      ->where('personas.estado','=','1')
       ->count();
+
+//-------------------------------------------------------------------------
+        //consulta contactos
+        $contactos= DB::table('contactos')
+        ->select('contactos.id')
+      ->count();
+     
      
 //----------------------------------------------------------------------------
         $consultapersonadesac= DB::table('personas')
@@ -32,6 +38,17 @@ $fecha=date("Y-m-d",strtotime($fecha_actual."- 1 days"));
 //----------------------------------------------------------------------------
         $consultasolicitud= DB::table('solicituds')
         ->select('solucituds.id')
+        ->count();
+ //----------------------------------------------------------------------------
+        //consulta Solicitud Sin procesar
+        $solsinprocesar= DB::table('solicituds')
+        ->select('solucituds.id')
+        ->where('solicituds.estadofactibilidad_id','=','1')
+        ->count();
+
+ //consulta Solicitud factible
+        $solicitudfac= DB::table('solicituds')
+        ->select('solucituds.id')
         ->where('solicituds.estadofactibilidad_id','=','2')
         ->count();
 //----------------------------------------------------------------------------
@@ -40,6 +57,14 @@ $fecha=date("Y-m-d",strtotime($fecha_actual."- 1 days"));
         ->select('solucituds.id')
         ->where('solicituds.estadofactibilidad_id','=','3')
         ->count();
+
+//----------------------------------------------------------------------------
+        //consulta Solicitud en proceso
+        $solproceso= DB::table('solicituds')
+        ->select('solucituds.id')
+        ->where('solicituds.estadofactibilidad_id','=','4')
+        ->count();
+
 //-------------------------------------------------------------------------------
         $licencia= DB::table('licencias')
         ->select('licencias.id')
@@ -50,8 +75,28 @@ $fecha=date("Y-m-d",strtotime($fecha_actual."- 1 days"));
         ->select('licencias.id')
         ->where('licencias.estadolicencia_id','=','2')
         ->count();
-        return view('admin.dashboard',compact('consulta','consultapersona','consultapersonadesac','consultasolicitud','consultasolicitudnofac','licencia','licenciadesc'));
+
+        $fecha_actual = date("Y-m-d");
+//sumo 1 día
+//$fechamas= date("d-m-Y",strtotime($fecha_actual."+ 1 days")); 
+//resto 1 día
+$fechamenos= date("Y-m-d",strtotime($fecha_actual."- 1 days")); 
+
+        $denuncia=DB::table('denuncias')
+        ->join('estadodenuncias','denuncias.estadodenuncia_id','=','estadodenuncias.id')
+        ->select('denuncias.descripcion','denuncias.fecha','denuncias.direccion','estadodenuncias.descripcion as d')
+        ->where('estadodenuncia_id','=','1')
+        ->whereBetween('fecha', [$fechamenos,$fecha_actual])
+        ->get();
+        //print_r($denuncia);
+        return view('admin.dashboard',compact('consulta','consultapersona',
+        'consultapersonadesac','consultasolicitud',
+        'consultasolicitudnofac','licencia','licenciadesc',
+        'denuncia', 'contactos', 'solsinprocesar','solicitudfac','solproceso'));
+       
         }
 
+
+      
        
 }
