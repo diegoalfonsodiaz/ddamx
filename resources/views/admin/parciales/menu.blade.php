959421e2d7385e4724c968c55e67bf2eaacb57d6
danilopez97 <estu97dani@gmail.com>
2019-10-29 22:58:40 -0600

    arreglos

--- resources/views/admin/parciales/menu.blade.php
+++ resources/views/admin/parciales/menu.blade.php
@@ -7,7 +7,7 @@
         <li {{ request()->is('licencia') ? 'class=active' : '' }}><a href="{{ url('/licencia') }}"> <i class="fa  fa-file-word-o"></i> <span>  Licencias </span> </a></li>
         <li {{ request()->is('denuncia') ? 'class=active' : '' }}><a href="{{ url('/denuncia') }}"> <i class="fa fa-bullhorn"></i> <span> Denuncias </span> </a></li>
         <li {{ request()->is('bitacora') ? 'class=active' : '' }}><a href="{{ url('/bitacora') }}"> <i class="fa fa-desktop"></i> <span>Bitácora  </span> </a></li>        
-        
+        @if (auth()->user()->hasRole(['admin']))
         <li class="treeview ">
           <a href="#"><i class="fa fa-hand-lizard-o"></i> <span>Mantenimiento</span>
             <span class="pull-right-container">
@@ -16,16 +16,17 @@
           </a>
           <ul class="treeview-menu">
             <li {{ request()->is('cargoejecutor') ? 'class=active' : '' }}><a href="{{ url('/cargoejecutor') }} ">Cargos de ejecutor</a></li>
-            @if (auth()->user()->hasRole(['admin']))
+          
             <li {{ request()->is('estadofactibilidad') ? 'class=active' : '' }}><a href=" {{ url('/estadofactibilidad') }} ">Estados de factibilidad</a></li>
             <li {{ request()->is('estadolicencia') ? 'class=active' : '' }}><a href="{{ url('/estadolicencia') }}">Estados de licencias</a></li>
             <li {{ request()->is('estadodenuncia') ? 'class=active' : '' }}><a href="{{route('estadodenuncias.index')}}">Estados de denuncias</a></li>
             <li {{ request()->is('tipoobra') ? 'class=active' : '' }}><a href="{{ url('/tipoobra') }} ">Tipos de obra</a></li>
             <li {{ request()->is('tipovia') ? 'class=active' : '' }}><a href="{{ url('/tipovia') }}">Tipos de vía </a></li>
             <li {{ request()->is('asunto') ? 'class=active' : '' }}><a href="{{ url('/asunto') }}">Asuntos de bitácora</a></li>
-            @endif
+           
           </ul>
         </li>
+        @endif
         
         <li class="treeview {{request()->is('/licencia') ? 'active' :''}}">
           <a href="#"><i class="fa fa-book"></i> <span>Seguimientos</span>
@@ -36,13 +37,6 @@
           <ul class="treeview-menu">
           <li {{ request()->is('ticketdenunciacreate') ? 'class=active' : '' }}><a href="{{ url('/ticketdenunciacreate') }}">Seguimiento de denuncia</a></li>
           <li {{ request()->is('ticketcreate') ? 'class=active' : '' }}><a href="{{ url('/ticketcreate') }}">Seguimiento de bitácora</a></li>
-            @if (auth()->user()->hasRole(['admin']))
-           
-            @endif
-           
-            
-         
-        
           </ul>
         </li>
 
