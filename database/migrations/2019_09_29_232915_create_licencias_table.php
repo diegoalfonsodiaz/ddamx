b2ca828c9592ccf668d0313b2ef4f714dc0f6f8b
danilopez97 <estu97dani@gmail.com>
2019-11-04 22:47:32 -0600

    modificacion

--- database/migrations/2019_09_29_232915_create_licencias_table.php
+++ database/migrations/2019_09_29_232915_create_licencias_table.php
@@ -15,13 +15,13 @@ class CreateLicenciasTable extends Migration
     {
         Schema::create('licencias', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('numerolicencia')->nullable();;
-            $table->date('fechaautorizacion')->nullable();;
-            $table->string('recibo')->nullable();;
-            $table->string('monto')->nullable();;
+            $table->string('numerolicencia')->nullable();
+            $table->date('fechaautorizacion')->nullable();
+            $table->string('recibo')->nullable();
+            $table->string('monto')->nullable();
             $table->string('derecho');
             $table->string('remocion');
-            $table->date('fechaconexion')->nullable();;
+            $table->date('fechaconexion')->nullable();
             $table->unsignedBigInteger('estadolicencia_id');
             $table->unsignedBigInteger('solicitudfactibilidad_id')->unique();
             $table->unsignedBigInteger('tipovia_id');
