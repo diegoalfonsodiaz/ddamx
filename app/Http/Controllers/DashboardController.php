9f11e30cf74ced34bbfb14baf950172301d0f7f8
danilopez97 <estu97dani@gmail.com>
2019-11-05 18:55:03 -0600

    roles

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
     {
         $this->middleware('auth');
     }
-    public function dashboard()
+    public function dashboard(Request $request)
     {
         $request->user()->autorizeRoles(['secretaria','operaciones','jefeoperaciones','admin']);
 
