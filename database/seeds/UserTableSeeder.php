2efbaa17b5f99907dc341fadd6196d5454080ade
danilopez97 <estu97dani@gmail.com>
2019-11-06 14:12:59 -0600

    rol

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,38 +14,15 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         $role_admin = Role::where('nombre','admin')->first();
-        $role_jefeoperaciones = Role::where('nombre','jefeoperaciones')->first();
-        $role_operaciones = Role::where('nombre','operaciones')->first();
-        $role_secretaria = Role::where('nombre','secretaria')->first();
+       
 
         $user = new User();
-        $user->name = "admin";
-        $user->email = "admin@gmail.com";
-        $user->password = bcrypt('administrador');
+        $user->name = "director";
+        $user->email = "dir@gmail.com";
+        $user->password = bcrypt('ddamxdirxela19');
         $user->save();
         $user->roles()->attach($role_admin);
 
-        $user = new User();
-        $user->name = "secretaria";
-        $user->email = "secretaria@gmail.com";
-        $user->password = bcrypt('secretaria');
-        $user->save();
-        $user->roles()->attach($role_secretaria);
-
-
-        $user = new User();
-        $user->name = "operaciones";
-        $user->email = "operaciones@gmail.com";
-        $user->password = bcrypt('operaciones');
-        $user->save();
-        $user->roles()->attach($role_operaciones);
-
-        $user = new User();
-        $user->name = "jefeoperaciones";
-        $user->email = "jefeoperaciones@gmail.com";
-        $user->password = bcrypt('jefeoperaciones');
-        $user->save();
-        $user->roles()->attach($role_jefeoperaciones);
 
     }
 }
